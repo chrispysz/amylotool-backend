721328663c0f28eea5f2a3dfa01f5a8388151c7d
chrispysz <krzysztof.pysz99@gmail.com>
2022-09-23 02:04:19 +0200

    cors

--- api/__init__.py
+++ api/__init__.py
@@ -1,13 +1,16 @@
 from flask import Flask
 from firebase_admin import credentials, initialize_app
+from flask_cors import CORS
 
 cred = credentials.Certificate("api/key.json")
 default_app = initialize_app(cred)
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
     app.config['SECRET_KEY'] = '0934asbfgds43dfshf5432'
     app.config['TIMEOUT'] = None
+    
 
     from .workspacesAPI import workspacesAPI
 
