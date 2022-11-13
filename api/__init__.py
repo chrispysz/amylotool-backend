8ef6af8ebcffff74d317ac5bff320356d7d9c529
chrispysz <krzysztof.pysz99@gmail.com>
2022-11-13 01:12:27 +0100

    removed firebase, specified versions

--- api/__init__.py
+++ api/__init__.py
@@ -1,9 +1,5 @@
 from flask import Flask
-from firebase_admin import credentials, initialize_app
 from flask_cors import CORS
-
-cred = credentials.Certificate("api/key.json")
-default_app = initialize_app(cred)
 
 def create_app():
     app = Flask(__name__)
@@ -11,12 +7,8 @@ def create_app():
     app.config['SECRET_KEY'] = '0934asbfgds43dfshf5432'
     app.config['TIMEOUT'] = None
     
-
-    from .workspacesAPI import workspacesAPI
     from .predictionsAPI import predictionsAPI
 
-    app.register_blueprint(workspacesAPI, url_prefix='/workspace')
     app.register_blueprint(predictionsAPI, url_prefix='/predict')
     
-
     return app
