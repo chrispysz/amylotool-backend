894a2baf12214640907cc1ed9710f9b275e047f4
chrispysz <krzysztof.pysz99@gmail.com>
2023-04-03 00:23:00 +0200

    change ambert url to azure

--- api/predictionsAPI.py
+++ api/predictionsAPI.py
@@ -9,7 +9,7 @@ predictionsAPI = Blueprint('predictionsAPI', __name__)
 @predictionsAPI.route('/model', methods=['POST'])
 @cross_origin()
 def model():
-    available_models = [{"model":"AmBERT", "url":"https://amylotool-ambert.herokuapp.com/predict/full"},
+    available_models = [{"model":"AmBERT", "url":"https://amylotool-ambert.azurewebsites.net/predict/full"},
     {"model":"ProteinBERT", "url":"https://amylotool-proteinbert.azurewebsites.net/predict/full"}]
 
     try:
